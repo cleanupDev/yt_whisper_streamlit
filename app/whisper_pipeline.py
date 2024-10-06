1f856548400f98cece9a0ec010fdd0846d5f78f1
cleanupDev <philip.goddinger@gmail.com>
2024-10-06 13:11:16 +0200

    add: mps support for whisper

--- app/whisper_pipeline.py
+++ app/whisper_pipeline.py
@@ -3,8 +3,15 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 
 def get_whisper_pipe():
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    if torch.backends.mps.is_available():
+        device = "mps"
+        torch_dtype = torch.float32  # MPS currently supports float32
+    elif torch.cuda.is_available():
+        device = "cuda:0"
+        torch_dtype = torch.float16
+    else:
+        device = "cpu"
+        torch_dtype = torch.float32
 
     model_id = "openai/whisper-large-v3-turbo"
 
